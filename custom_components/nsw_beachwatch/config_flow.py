e0cc4e6b4216d34139564890aa65fd8499b9c4d9
PlanetCitizen1829381 <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-01-30 19:49:37 +1100

    Update config_flow.py

--- custom_components/nsw_beachwatch/config_flow.py
+++ custom_components/nsw_beachwatch/config_flow.py
@@ -10,15 +10,12 @@ class BeachwatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        errors = {}
         api = BeachwatchAPI(async_get_clientsession(self.hass))
         beaches = await api.get_all_beaches()
-
         if user_input is not None:
             await self.async_set_unique_id(user_input["beach_name"])
             self._abort_if_unique_id_configured()
             return self.async_create_entry(title=user_input["beach_name"], data=user_input)
-
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
@@ -26,7 +23,6 @@ class BeachwatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     SelectSelectorConfig(options=beaches, mode=SelectSelectorMode.DROPDOWN, custom_value=True)
                 ),
             }),
-            errors=errors,
         )
 
     @staticmethod
@@ -41,10 +37,12 @@ class BeachwatchOptionsFlowHandler(config_entries.OptionsFlow):
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
-
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
-                vol.Optional("update_interval", default=30): vol.All(vol.Coerce(int), vol.Range(min=1, max=1440)),
+                vol.Optional(
+                    "update_interval", 
+                    default=self.config_entry.options.get("update_interval", 30)
+                ): vol.All(vol.Coerce(int), vol.Range(min=5, max=1440)),
             })
         )
