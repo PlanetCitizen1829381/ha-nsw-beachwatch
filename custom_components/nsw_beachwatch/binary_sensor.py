d269b97942d650ef6b804f74005a74e375c8717e
PlanetCitizen1829381 <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-01-30 19:46:24 +1100

    Refactor NSWBeachwatchBinarySensor attributes and logic

--- custom_components/nsw_beachwatch/binary_sensor.py
+++ custom_components/nsw_beachwatch/binary_sensor.py
@@ -1,7 +1,5 @@
-from homeassistant.components.binary_sensor import (
-    BinarySensorEntity,
-    BinarySensorDeviceClass,
-)
+from homeassistant.components.binary_sensor import BinarySensorEntity, BinarySensorDeviceClass
+from homeassistant.helpers.entity import DeviceInfo
 from .const import DOMAIN
 
 async def async_setup_entry(hass, entry, async_add_entities):
@@ -9,22 +7,20 @@ async def async_setup_entry(hass, entry, async_add_entities):
     async_add_entities([NSWBeachwatchBinarySensor(beach_name)], True)
 
 class NSWBeachwatchBinarySensor(BinarySensorEntity):
+    _attr_has_entity_name = True
+    _attr_name = "Swimming Safety"
+
     def __init__(self, beach_name):
         self._beach_name = beach_name
-        self._attr_name = f"{beach_name} Swimming Safety"
-        self._attr_unique_id = f"bw_safety_{beach_name.lower().replace(' ', '_')}"
+        self._attr_unique_id = f"bw_safe_{beach_name.lower().replace(' ', '_')}"
         self._attr_device_class = BinarySensorDeviceClass.SAFETY
-        self._state = False
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, beach_name)},
+            name=beach_name,
+        )
 
     @property
     def is_on(self):
-        entity_id = f"sensor.beachwatch_{self._beach_name.lower().replace(' ', '_')}"
-        main_sensor = self.hass.states.get(entity_id)
-        
-        if main_sensor:
-            if "Likely" in main_sensor.state:
-                return True
-        return False
-
-    async def async_update(self):
-        pass
+        entity_id = f"sensor.beachwatch_{self._beach_name.lower().replace(' ', '_')}_status"
+        state = self.hass.states.get(entity_id)
+        return state and "Likely" in state.state
