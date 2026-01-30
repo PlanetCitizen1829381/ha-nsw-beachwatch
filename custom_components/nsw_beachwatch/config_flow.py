b3f2201bba0271d91a07b2bd018ac12160b68944
PlanetCitizen1829381 <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-01-30 21:41:42 +1100

    Update config_flow.py

--- custom_components/nsw_beachwatch/config_flow.py
+++ custom_components/nsw_beachwatch/config_flow.py
@@ -1,48 +1,49 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.selector import SelectSelector, SelectSelectorConfig, SelectSelectorMode
-from .api import BeachwatchAPI
+from .api import NSWBeachwatchAPI
 from .const import DOMAIN
 
-class BeachwatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class NSWBeachwatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        api = BeachwatchAPI(async_get_clientsession(self.hass))
+        errors = {}
+        api = NSWBeachwatchAPI()
         beaches = await api.get_all_beaches()
+
         if user_input is not None:
-            await self.async_set_unique_id(user_input["beach_name"])
+            await self.async_set_unique_id(user_input["beach_name"].lower().replace(" ", "_"))
             self._abort_if_unique_id_configured()
             return self.async_create_entry(title=user_input["beach_name"], data=user_input)
+
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required("beach_name"): SelectSelector(
-                    SelectSelectorConfig(options=beaches, mode=SelectSelectorMode.DROPDOWN, custom_value=True)
-                ),
+                vol.Required("beach_name"): vol.In(beaches)
             }),
+            errors=errors,
         )
 
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
-        return BeachwatchOptionsFlowHandler(config_entry)
+        return NSWBeachwatchOptionsFlowHandler(config_entry)
 
-class BeachwatchOptionsFlowHandler(config_entries.OptionsFlow):
+class NSWBeachwatchOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
+
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
                 vol.Optional(
-                    "update_interval", 
-                    default=self.config_entry.options.get("update_interval", 30)
-                ): vol.All(vol.Coerce(int), vol.Range(min=5, max=1440)),
-            })
+                    "update_interval",
+                    default=self.config_entry.options.get("update_interval", 30),
+                ): vol.All(vol.Coerce(int), vol.Range(min=1, max=1440)),
+            }),
         )
