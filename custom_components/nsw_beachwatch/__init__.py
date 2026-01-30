0b41f7615af21a5bba73f6d9da6287da52952ffd
PlanetCitizen1829381 <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-01-30 21:06:46 +1100

    Update __init__.py

--- custom_components/nsw_beachwatch/__init__.py
+++ custom_components/nsw_beachwatch/__init__.py
@@ -1,28 +1,24 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from .api import BeachwatchAPI
+from .api import NSWBeachwatchAPI
 from .const import DOMAIN
 
-PLATFORMS = ["sensor", "binary_sensor"]
+PLATFORMS: list[str] = ["sensor", "binary_sensor"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    session = async_get_clientsession(hass)
-    api = BeachwatchAPI(session)
+    api = NSWBeachwatchAPI()
     
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = api
-    
-    entry.async_on_unload(entry.add_update_listener(update_listener))
-    
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    return True
 
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_reload(entry.entry_id)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
+
     return unload_ok
