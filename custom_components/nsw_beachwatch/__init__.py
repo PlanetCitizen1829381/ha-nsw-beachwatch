75dce9987b9a2aefa76cae5763265bd9d022c7d5
PlanetCitizen1829381 <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-01-30 15:37:14 +1100

    Comment out sensor setup and unload lines
    Comment out sensor platform setup and unload lines until sensor.py is created.

--- custom_components/nsw_beachwatch/__init__.py
+++ custom_components/nsw_beachwatch/__init__.py
@@ -10,10 +10,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up NSW Beachwatch from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     
-    # Forward the setup to the sensor platform
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    # COMMENT OUT the line below until we actually create sensor.py
+    # await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    # COMMENT OUT the line below as well
+    # return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    return True
