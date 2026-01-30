698a8e6c4977a5a9ffef850dbd675d1a9ab29b78
Ramen4Life <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-01-31 09:05:42 +1100

    Update config_flow.py

--- custom_components/nsw_beachwatch/config_flow.py
+++ custom_components/nsw_beachwatch/config_flow.py
@@ -1,66 +1,48 @@
-import aiohttp
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.helpers.selector import (
-    SelectSelector,
-    SelectSelectorConfig,
-    SelectSelectorMode,
-)
+from homeassistant.core import callback
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.selector import SelectSelector, SelectSelectorConfig, SelectSelectorMode
+from .api import BeachwatchAPI
 from .const import DOMAIN
 
 class BeachwatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-
     VERSION = 1
 
-    async def _get_beaches(self):
-        url = "https://api.beachwatch.nsw.gov.au/public/sites/geojson"
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, timeout=10) as response:
-                    if response.status == 200:
-                        data = await response.json()
-                        beaches = sorted({
-                            feature["properties"]["siteName"]
-                            for feature in data.get("features", [])
-                            if feature["properties"].get("siteName")
-                        })
-                        return beaches
-        except Exception:
-            return []
-        return []
-
     async def async_step_user(self, user_input=None):
-        errors = {}
-        beaches = await self._get_beaches()
-
+        api = BeachwatchAPI(async_get_clientsession(self.hass))
+        beaches = await api.get_all_beaches()
         if user_input is not None:
-            selected_beach = user_input["beach_name"]
-            
-            if selected_beach not in beaches:
-                errors["beach_name"] = "invalid_beach"
-            else:
-                await self.async_set_unique_id(selected_beach)
-                self._abort_if_unique_id_configured()
-                
-                return self.async_create_entry(
-                    title=selected_beach, 
-                    data={"beach_name": selected_beach}
-                )
-
-        if not beaches:
-            errors["base"] = "cannot_connect"
-            beaches = ["Bondi Beach", "Manly Ocean Beach"]
-
+            await self.async_set_unique_id(user_input["beach_name"])
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(title=user_input["beach_name"], data=user_input)
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required("beach_name"): SelectSelector(
-                    SelectSelectorConfig(
-                        options=beaches,
-                        mode=SelectSelectorMode.DROPDOWN,
-                        custom_value=True,
-                    )
+                    SelectSelectorConfig(options=beaches, mode=SelectSelectorMode.DROPDOWN, custom_value=True)
                 ),
             }),
-            errors=errors,
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return BeachwatchOptionsFlowHandler(config_entry)
+
+class BeachwatchOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Optional(
+                    "update_interval", 
+                    default=self.config_entry.options.get("update_interval", 30)
+                ): vol.All(vol.Coerce(int), vol.Range(min=5, max=1440)),
+            })
         )
