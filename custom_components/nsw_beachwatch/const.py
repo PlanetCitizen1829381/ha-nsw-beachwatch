f10b1a9b2b0a91d8cbfab32f46e869eafb7962d6
PlanetCitizen1829381 <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-01-30 14:02:47 +1100

    Update constants.

--- custom_components/nsw_beachwatch/const.py
+++ custom_components/nsw_beachwatch/const.py
@@ -1,8 +1,19 @@
-"""Constants for integration_blueprint."""
+"""Constants for the NSW Beachwatch integration."""
 
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "integration_blueprint"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+NAME = "NSW Beachwatch"
+DOMAIN = "nsw_beachwatch"
+VERSION = "1.0.0"
+
+# API Configuration
+# This is the base URL for the NSW Beachwatch GeoJSON feed
+API_URL = "https://api.beachwatch.nsw.gov.au/public/sites/geojson"
+
+# Default polling interval in minutes
+# We don't need to check every second; every 30 minutes is plenty for beach data
+SCAN_INTERVAL_MINUTES = 30
+
+CONF_BEACH_NAME = "beach_name"
