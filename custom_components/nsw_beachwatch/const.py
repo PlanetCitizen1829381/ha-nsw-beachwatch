b4a8b2336a15c33ff8085ad24899f4898910a5d7
PlanetCitizen1829381 <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-01-30 15:33:22 +1100

    Remove unused constants and logging imports

--- custom_components/nsw_beachwatch/const.py
+++ custom_components/nsw_beachwatch/const.py
@@ -1,19 +1,4 @@
 """Constants for the NSW Beachwatch integration."""
 
-from logging import Logger, getLogger
-
-LOGGER: Logger = getLogger(__package__)
-
-NAME = "NSW Beachwatch"
 DOMAIN = "nsw_beachwatch"
-VERSION = "1.0.0"
-
-# API Configuration
-# This is the base URL for the NSW Beachwatch GeoJSON feed
-API_URL = "https://api.beachwatch.nsw.gov.au/public/sites/geojson"
-
-# Default polling interval in minutes
-# We don't need to check every second; every 30 minutes is plenty for beach data
-SCAN_INTERVAL_MINUTES = 30
-
 CONF_BEACH_NAME = "beach_name"
