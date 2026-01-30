d26a78d1b336a5fe75fd7fdc3c293145a7f56bbf
PlanetCitizen1829381 <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-01-30 15:33:48 +1100

    Refactor config flow to simplify beach name input

--- custom_components/nsw_beachwatch/config_flow.py
+++ custom_components/nsw_beachwatch/config_flow.py
@@ -1,12 +1,6 @@
-"""Config flow for NSW Beachwatch integration."""
-from __future__ import annotations
-
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
-from homeassistant.helpers import selector
-
-from .const import CONF_BEACH_NAME, DOMAIN
+from .const import DOMAIN, CONF_BEACH_NAME
 
 class NSWBeachwatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for NSW Beachwatch."""
@@ -18,26 +12,17 @@ class NSWBeachwatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            # Here we create the entry. The title will be the Beach Name.
             return self.async_create_entry(
                 title=user_input[CONF_BEACH_NAME], 
                 data=user_input
             )
 
-        # This defines the form the user sees
-        data_schema = vol.Schema(
-            {
-                vol.Required(CONF_BEACH_NAME): selector.TextSelector(
-                    selector.TextSelectorConfig(
-                        type=selector.TextSelectorType.TEXT,
-                        placeholder="e.g. Bondi Beach"
-                    ),
-                ),
-            }
-        )
-
         return self.async_show_form(
-            step_id="user", 
-            data_schema=data_schema, 
-            errors=errors
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_BEACH_NAME): str,
+                }
+            ),
+            errors=errors,
         )
