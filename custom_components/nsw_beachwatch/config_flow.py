f0f378d4dcc3642cd1f766b45f750f1cd313ec16
Ramen4Life <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-01-31 11:48:58 +1100

    Update config_flow.py

--- custom_components/nsw_beachwatch/config_flow.py
+++ custom_components/nsw_beachwatch/config_flow.py
@@ -20,8 +20,6 @@ class NswBeachwatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             self._abort_if_unique_id_configured()
             return self.async_create_entry(title=user_input["beach_name"], data=user_input)
 
-        # We use a dictionary-based schema without vol.Required/Optional 
-        # to prevent the UI from auto-selecting the first item.
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
@@ -39,15 +37,13 @@ class NswBeachwatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
-        return NswBeachwatchOptionsFlowHandler(config_entry)
+        return NswBeachwatchOptionsFlowHandler()
 
 class NswBeachwatchOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
+            
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
