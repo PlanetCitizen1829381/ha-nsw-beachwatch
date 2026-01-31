6cc07d97aefe0297098e434d8b6957f79797c111
Ramen4Life <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-01-31 11:49:29 +1100

    Update __init__.py

--- custom_components/nsw_beachwatch/__init__.py
+++ custom_components/nsw_beachwatch/__init__.py
@@ -1,58 +1,25 @@
-import voluptuous as vol
-from homeassistant import config_entries
-from homeassistant.core import callback
-from homeassistant.helpers.selector import SelectSelector, SelectSelectorConfig, SelectSelectorMode
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from .api import NSWBeachwatchAPI
 from .const import DOMAIN
 
-class NswBeachwatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
+PLATFORMS = ["sensor"]
 
-    async def async_step_user(self, user_input=None):
-        api = NSWBeachwatchAPI()
-        beaches = await api.get_all_beaches()
-        
-        if not beaches:
-            return self.async_abort(reason="cannot_connect")
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    api = NSWBeachwatchAPI()
+    
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = api
+    
+    entry.async_on_unload(entry.add_update_listener(update_listener))
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
 
-        if user_input is not None:
-            await self.async_set_unique_id(user_input["beach_name"])
-            self._abort_if_unique_id_configured()
-            return self.async_create_entry(title=user_input["beach_name"], data=user_input)
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_reload(entry.entry_id)
 
-        # Using a raw string key instead of vol.Required helps the UI start blank
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                "beach_name": SelectSelector(
-                    SelectSelectorConfig(
-                        options=beaches, 
-                        mode=SelectSelectorMode.DROPDOWN, 
-                        custom_value=False,
-                        sort=True
-                    )
-                ),
-            }),
-        )
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        return NswBeachwatchOptionsFlowHandler(config_entry)
-
-class NswBeachwatchOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema({
-                vol.Optional(
-                    "update_interval", 
-                    default=self.config_entry.options.get("update_interval", 30)
-                ): vol.All(vol.Coerce(int), vol.Range(min=5, max=1440)),
-            })
-        )
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
