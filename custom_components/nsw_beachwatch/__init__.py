fa251eff0d96b734ec89524b49ab7a4b5a6721f7
Ramen4Life <148546637+PlanetCitizen1829381@users.noreply.github.com>
2026-02-02 10:49:58 +1100

    Update __init__.py

--- custom_components/nsw_beachwatch/__init__.py
+++ custom_components/nsw_beachwatch/__init__.py
@@ -32,7 +32,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})\n    hass.data[DOMAIN][entry.entry_id] = coordinator
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
 
     entry.async_on_unload(entry.add_update_listener(update_listener))
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
